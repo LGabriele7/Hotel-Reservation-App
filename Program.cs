6607ebef4bb087088d089e7a7ec8a9445c1cbf27
LGabriele7 <lancegabmendoza@gmail.com>
2026-05-25 13:38:20 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hotel_Reservation_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
