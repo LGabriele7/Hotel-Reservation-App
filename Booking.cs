2687a15da49fa51693292e61f452faa912b2458f
LGabriele7 <lancegabmendoza@gmail.com>
2026-06-10 18:55:57 +1200

    Adding Methods

--- Booking.cs
+++ Booking.cs
@@ -4,24 +4,13 @@ using System.Text;
 
 namespace Hotel_Reservation_App
 {
-    public class Booking
+    internal class Booking
     {
         public string GuestName { get; set; }
         public int RoomNumber { get; set; }
-        public string RoomType { get; set; }
-        public decimal Price { get; set; }
-        public DateTime CheckInDate { get; set; }
-        public DateTime CheckOutDate { get; set; }
-
-        public Booking(string guestName, int roomNumber, string roomType,
-                       decimal price, DateTime checkInDate, DateTime checkOutDate)
-        {
-            GuestName = guestName;
-            RoomNumber = roomNumber;
-            RoomType = roomType;
-            Price = price;
-            CheckInDate = checkInDate;
-            CheckOutDate = checkOutDate;
-        }
-    }// end of class
+        public string CheckInDate { get; set; }
+        public string CheckOutDate { get; set; }
+    }
+}
+}// end of class
 }// end of namespace
--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@
     internal class Program
     {
         static List<User> users = new List<User>(); 
-        static List<Room> rooms = new List<Room> (); 
+        static List<Room> rooms = new List<Room> ();
+        static List<Booking> bookings = new List<Booking>(); 
 
         static void Main(string[] args) 
         {
@@ -48,5 +49,103 @@
             Console.WriteLine("Username: ");
             Console.WriteLine("Password: ");
         }
-    }
-}
+
+        //Create Booking Method
+        public static void CreateBooking()
+        {
+            Booking booking = new Booking();
+
+            Console.WriteLine("\n--------- Create Booking ---------");
+
+            Console.Write("Guest Name: ");
+            booking.GuestName = Console.ReadLine();
+
+            Console.Write("Room Number: ");
+            booking.RoomNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Check-In Date: ");
+            booking.CheckInDate = Console.ReadLine();
+
+            Console.Write("Check-Out Date: ");
+            booking.CheckOutDate = Console.ReadLine();
+
+            bookings.Add(booking);
+
+            Console.WriteLine("\nBooking Created Successfully!");
+        }//end of Create Booking Method
+
+        //Guest Menu
+        public static void GuestMenu()
+        {
+            int option;
+
+            do
+            {
+                Console.WriteLine("\n--------- Guest Menu ---------");
+                Console.WriteLine("1. View Rooms");
+                Console.WriteLine("2. Create Booking");
+                Console.WriteLine("3. Logout");
+
+                Console.Write("Select Option: ");
+                option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Display Rooms Here");
+                        break;
+
+                    case 2:
+                        CreateBooking();
+                        break;
+                }
+
+            } while (option != 3);
+        }//end of Guest Menu
+
+        //Manage Booking Method
+        public static void ManageBooking()
+        {
+            Console.WriteLine("\n--------- Manage Booking ---------");
+
+            if (bookings.Count == 0)
+            {
+                Console.WriteLine("No bookings found.");
+                return;
+            }
+
+            for (int i = 0; i < bookings.Count; i++)
+            {
+                Console.WriteLine(
+                    $"{i + 1}. {bookings[i].GuestName} | Room {bookings[i].RoomNumber}");
+            }
+        }//end of Manage Booking Method
+
+        //Admin Menu
+        public static void AdminMenu()
+        {
+            int option;
+
+            do
+            {
+                Console.WriteLine("\n--------- Admin Menu ---------");
+                Console.WriteLine("1. Manage Rooms");
+                Console.WriteLine("2. Manage Guest");
+                Console.WriteLine("3. Manage Booking");
+                Console.WriteLine("4. Exit");
+
+                Console.Write("Select Option: ");
+                option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 3:
+                        ManageBooking();
+                        break;
+                }
+
+            } while (option != 4);
+        }//end of Admin Menu
+
+    }//end of class
+}//end of namespace
